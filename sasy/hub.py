f4c0313b7c1e242c9e5a4d372dc32cc3acefc256
nassim babaci <nassim@nba.ifr.lan>
2013-07-19 14:58:54 +0200

    added hub module

---
+++ sasy/hub.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python -O
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 WALLIX, SARL. All rights reserved.
+#
+# Licensed computer software. Property of WALLIX.
+# Product name: WALLIX Admin Bastion V 3.0
+# Author(s): Nassim Babaci <nbabaci@wallix.com>
+# Id: $Id$
+# URL: $URL$
+# Module description:  $(description)
+
+from interface import IHub, IStorage
+from zope.component import implements, adapts
+
